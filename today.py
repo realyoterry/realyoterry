ad59a305aa822933b14a5c0fb6f771cd4f2e0a4e
realyoterry <theterrykim@gmail.com>
2025-02-15 20:08:50 +1300

    fix: errors

--- today.py
+++ today.py
@@ -7,7 +7,7 @@ import time
 import hashlib
 
 
-HEADERS = {'authorization': 'token '+ os.getenv('ACCESS_TOKEN')}
+HEADERS = {'authorization': 'token'+ os.getenv('ACCESS_TOKEN')}
 USER_NAME = os.getenv('USER_NAME')
 QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 'recursive_loc': 0, 'graph_commits': 0, 'loc_query': 0}
 
